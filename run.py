c8319a018a440b15c644bcf63c6bd94d2931782e
Lee Chun Hoe <leechunhoe01@gmail.com>
2020-08-16 10:40:45 +0800

    dev: group by id

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import csv
+from itertools import groupby
 
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     data = [row for row in csv_reader if len(row) > 0]
-    print(data)
+    grouped_data = [list(i) for j, i in groupby(data, lambda a: a[0])]
+    for row in grouped_data:
+    	print(row[0][0])
 
 # f = open("test.md", "w")
 # f.write("Created file")
